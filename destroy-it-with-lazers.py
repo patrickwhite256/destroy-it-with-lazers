efdac20b081e1793b758d5356e0295c5f9c1573c
Patrick White <patrickwhite.256@gmail.com>
2014-07-11 23:10:02 -0400

    working with all angles

--- destroy-it-with-lazers.py
+++ destroy-it-with-lazers.py
@@ -5,35 +5,6 @@ from pygame.locals import *
 from numpy import matrix
 from numpy import linalg
 
-NUMSTATES = 3
-class GameRect:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.state = 0
-        self.wall = None
-    def draw(self, display):
-        border = (0,0,0)
-        fill = (200,200,200)
-        line = (0,255,0)
-        pygame.draw.rect(display, fill, (self.x * 40, self.y * 40, 40, 40), 0)
-        if self.state == 0:
-            self.wall = None
-            pass
-        elif self.state == 1:
-            self.wall = LineSegment((self.x * 40, self.y * 40), ((self.x + 1) * 40, (self.y + 1) * 40))
-        elif self.state == 2:
-            self.wall = LineSegment((self.x * 40, (self.y + 1) * 40), ((self.x + 1) * 40, self.y * 40))
-        if self.wall != None:
-            self.wall.draw(display,line)
-        pygame.draw.rect(display, border, (self.x * 40, self.y * 40, 40, 40), 1)
-    def contains(self, pos):
-        if pos[0] > self.x * 40 and pos[0] < (self.x + 1) * 40 and pos[1] > self.y * 40 and pos[1] < (self.y + 1) * 40:
-            return True
-        return False
-    def shift(self):
-        self.state = (self.state + 1) % NUMSTATES
-
 class Line:
     def __init__(self, pos, dirvec):
         self.pos = pos
@@ -86,47 +57,40 @@ def computeLaserPath(walls):
             points.append(laserLine.at(minIntersection))
             lastWall = currentWall
             v = currentWall.line.dirvec
-            v = (v[1], v[0])
+            v = (-v[1], v[0])
             pmag = (direction[0] * v[0] + direction[1] * v[1])/(v[0]*v[0]+v[1]*v[1])
             proj = (v[0]*pmag,v[1]*pmag)
-            direction = (2*proj[0]-direction[0],2*proj[1]-direction[1])
-            print("new dir:", direction)
+            direction = (direction[0]-2*proj[0],direction[1]-2*proj[1])
     return points
 
 def main():
     pygame.init()
     pygame.display.set_caption("Destroy it with Lazers!")
-    screen = pygame.display.set_mode((240, 240))
-    rects = []
-    for i in range(36):
-        r = GameRect(i % 6, int(i / 6))
-        rects.append(r)
-    lasermoving = False
-
+    screen = pygame.display.set_mode((600, 600))
+    startpos = None
     laserpath = None
+    walls = []
     while True:
         ticks = pygame.time.get_ticks()
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             if event.type == MOUSEBUTTONDOWN:
-                for r in rects:
-                    if r.contains(event.pos):
-                        r.shift()
+                startpos = event.pos
+            if event.type == MOUSEBUTTONUP:
+                walls.append(LineSegment(startpos, event.pos))
+                startpos = None
             if event.type == KEYDOWN:
-                lasermoving = True
                 walls = []
-                for r in rects:
-                    if r.wall != None:
-                        walls.append(r.wall)
-                laserpath = computeLaserPath(walls)
-                print(laserpath)
+        laserpath = computeLaserPath(walls + ([LineSegment(startpos, pygame.mouse.get_pos())] if startpos != None else []))
 
-        screen.fill((255,255,255))
-        for r in rects:
-            r.draw(screen)
+        screen.fill((200,200,200))
+        for w in (walls):
+            w.draw(screen, (0,0,255))
         if laserpath != None:
             pygame.draw.lines(screen, (255,0,0), False, laserpath)
+        if startpos != None:
+            pygame.draw.line(screen, (0,255,0),startpos, pygame.mouse.get_pos())
 
         while pygame.time.get_ticks() - ticks < 16:
             pass
